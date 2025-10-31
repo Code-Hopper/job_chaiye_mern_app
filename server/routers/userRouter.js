1fadda563ea610d2b651eb92542bf1739ee8609f
“code-hopper” <ameykhondekar01@gmail.com>
2025-10-31 13:30:14 +0530

    password reset and login user

--- server/routers/userRouter.js
+++ server/routers/userRouter.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { test, handleUserRegister, handleOTPVerification } from "../controllers/userController.js"
+import { test, handleUserRegister, handleOTPVerification, handleUserLogin, handleResetPasswordRequest, handleOTPForPasswordReset } from "../controllers/userController.js"
 
 let userRouter = express.Router()
 
@@ -8,5 +8,11 @@ userRouter.get("/test", test)
 userRouter.post("/register", handleUserRegister)
 
 userRouter.post("/verify-otp", handleOTPVerification)
+
+userRouter.post("/user-login", handleUserLogin)
+
+userRouter.post("/password-reset-request", handleResetPasswordRequest)
+
+userRouter.post("/verify-reset-password-request", handleOTPForPasswordReset)
 
 export { userRouter }
