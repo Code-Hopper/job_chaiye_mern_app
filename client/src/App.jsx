52034b4a92d787b449faccb695eda37f867530b1
“code-hopper” <ameykhondekar01@gmail.com>
2025-11-10 14:19:06 +0530

    header and login register components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,23 +2,25 @@ import React from 'react'
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
+// pages
 import Home from "./components/pages/Home.jsx"
 import UserLoginRegister from './components/pages/UserLoginRegister.jsx'
+
+// context
+import { UserProvider } from './context/userContext.jsx'
 
 const App = () => {
 
   return (
     <>
-      {/* <JobProvider>
-        <UserPorovider> */}
-      <Router>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/user-login-register' element={<UserLoginRegister />} />
-        </Routes>
-      </Router>
-      {/* </UserPorovider>
-      </JobProvider> */}
+      <UserProvider>
+        <Router>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/user-login-register' element={<UserLoginRegister />} />
+          </Routes>
+        </Router>
+      </UserProvider>
     </>
   )
 }
