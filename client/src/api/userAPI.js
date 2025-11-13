2b61026c7e5d23e2632c85b7e641e0ddd7aaa606
“code-hopper” <ameykhondekar01@gmail.com>
2025-11-13 13:52:57 +0530

    dashboard and login

--- client/src/api/userAPI.js
+++ client/src/api/userAPI.js
@@ -20,3 +20,29 @@ export const requestUserEmailOtpVerification = async (data) => {
         throw err
     }
 }
+
+export const requestUserLogin = async (data) => {
+    try {
+        let result = await axios.post(`${baseUrl}/user-login`, data)
+        return result
+    } catch (err) {
+        throw err
+    }
+}
+
+export const requestUserProfile = async (token) => {
+    try {
+        let result = await axios({
+            method: "GET",
+            url: `${baseUrl}/fetch-urser-profile`,
+            headers: {
+                authorization: token
+            }
+        })
+
+        return result
+
+    } catch (err) {
+        throw (err)
+    }
+}
